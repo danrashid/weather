f66650861491b11c2d8089d6f13d9d2ebbcb6b03
Dan Rashid <dan.rashid@veriforce.com>
2023-06-27 10:45:19 -0500

    Fetch stills and write manifest

--- weather.sh
+++ weather.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 set -e
 
-DESTINATION=$1
+destination=$1
 
-LATEST=$(($(date +%s) / (60 * 20)))
-curl --silent https://radar.weather.gov/ridge/standard/CONUS-LARGE_loop.gif -o "$DESTINATION$LATEST".gif
+for i in {9..0}
+do
+  url="https://radar.weather.gov/ridge/standard/CONUS-LARGE_$i.gif"
+  etag=$(curl -sI $url | grep etag | sed -E 's/.+"(.+)".+/\1/')
+  filepath="$destination$etag.gif"
 
-ls -1tr "$DESTINATION"* | head -n-18 | xargs rm -f
+  if [ ! -f $filepath ]
+  then
+    curl -s $url -o $filepath
+  fi
+done
+
+ls -1tr "$destination"*.gif | head -n-90 | xargs rm -f
+ls -1tr $destination | grep gif > "$destination"manifest.txt
 
 exit 0
