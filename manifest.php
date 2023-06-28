95039b55e0d1cb313273ab439c3d001868b74eb2
Dan Rashid <dan@danrashid.com>
2023-06-27 19:11:07 -0500

    Reduce manifest

--- manifest.php
+++ manifest.php
@@ -1,11 +1,11 @@
 <?php
 
 $filepaths = glob('/var/www/html/images/*.gif');
-usort($filepaths, function($a, $b) {
+usort($filepaths, function ($a, $b) {
   return filemtime($a) - filemtime($b);
 });
 
 header('Content-type: text/plain');
-echo implode("\n", array_map('basename', $filepaths));
-
-?>
+echo implode("\n", array_map(function ($filepath) {
+  return basename($filepath, '.gif');
+}, $filepaths));
